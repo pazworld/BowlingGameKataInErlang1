5a1ff5af71bbad53d02b577f7907f57ebd06267c
pazworld <paz@moudamepo.net>
2020-10-17 17:11:23 +0900

    Red: Add test for all ones game

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -6,3 +6,9 @@ gutter_game_test() ->
     PinList = lists:duplicate(20, 0),
     G2 = lists:foldl(fun(_, Game) -> bowling_game:roll(Game, 0) end, G, PinList),
     ?assertEqual(0, bowling_game:score(G2)).
+
+all_ones_test() ->
+    G = bowling_game:new(),
+    PinList = lists:duplicate(20, 1),
+    G2 = lists:foldl(fun(Pins, Game) -> bowling_game:roll(Game, Pins) end, G, PinList),
+    ?assertEqual(20, bowling_game:score(G2)).
