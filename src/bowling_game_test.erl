15d7dc476365c98f56e776a7efd7fe28d8d4a260
pazworld <paz@moudamepo.net>
2020-10-17 18:19:28 +0900

    Refactor: Add roll_many()

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -3,12 +3,14 @@
 
 gutter_game_test() ->
     G = bowling_game:new(),
-    PinList = lists:duplicate(20, 0),
-    G2 = lists:foldl(fun(_, Game) -> bowling_game:roll(Game, 0) end, G, PinList),
+    G2 = roll_many(G, 20, 0),
     ?assertEqual(0, bowling_game:score(G2)).
 
 all_ones_test() ->
     G = bowling_game:new(),
-    PinList = lists:duplicate(20, 1),
-    G2 = lists:foldl(fun(Pins, Game) -> bowling_game:roll(Game, Pins) end, G, PinList),
+    G2 = roll_many(G, 20, 1),
     ?assertEqual(20, bowling_game:score(G2)).
+
+roll_many(Game, N, Pins) ->
+    PinList = lists:duplicate(N, Pins),
+    lists:foldl(fun(P, G) -> bowling_game:roll(G, P) end, Game, PinList).
