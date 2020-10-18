8c9f757a598db75d6c678bfa68c646d13de8a49c
pazworld <paz@moudamepo.net>
2020-10-18 15:37:02 +0900

    Red: Add test for a game with strikes

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -19,6 +19,14 @@ one_spare_test() ->
     G5 = roll_many(G4, 17, 0),
     ?assertEqual(16, bowling_game:score(G5)).
 
+one_strike_test() ->
+    G = bowling_game:new(),
+    G2 = bowling_game:roll(G, 10),
+    G3 = bowling_game:roll(G2, 3),
+    G4 = bowling_game:roll(G3, 4),
+    G5 = roll_many(G4, 16, 0),
+    ?assertEqual(24, bowling_game:score(G5)).
+
 roll_many(Game, N, Pins) ->
     PinList = lists:duplicate(N, Pins),
     lists:foldl(fun(P, G) -> bowling_game:roll(G, P) end, Game, PinList).
