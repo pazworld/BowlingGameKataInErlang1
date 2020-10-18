faed684efc5b04b912895ca1321b06fc83d2568a
pazworld <paz@moudamepo.net>
2020-10-18 15:37:29 +0900

    Green: It scores a game with strikes

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -11,8 +11,12 @@ score(Game) ->
         Pin1 = lists:nth(FrameIndex, Rolls),
         Pin2 = lists:nth(FrameIndex + 1, Rolls),
         IsSpare = is_spare(FrameIndex, Rolls),
-        if IsSpare ->
-            {FrameIndex + 2, Score + 10 + lists:nth(FrameIndex + 2, Rolls)}
+        if Pin1 =:= 10 ->
+            Pin3 = lists:nth(FrameIndex + 2, Rolls),
+            {FrameIndex + 1, Score + 10 + Pin2 + Pin3}
+        ;  IsSpare ->
+            Pin3 = lists:nth(FrameIndex + 2, Rolls),
+            {FrameIndex + 2, Score + 10 + Pin3}
         ;  Pin1 + Pin2 =/= 10 ->
             {FrameIndex + 2, Score + Pin1 + Pin2}
         end
