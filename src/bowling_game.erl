369a360e9ce8529f26056c5d0a6924f05bea806a
pazworld <paz@moudamepo.net>
2020-10-18 15:10:39 +0900

    Green: It scores a game with spares

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -7,12 +7,14 @@ roll(Game, Pins) -> [Pins | Game].
 
 score(Game) ->
     Rolls = lists:reverse(Game),
-    F = fun(_Frame, {FrameIndex, Sum}) ->
+    F = fun(_Frame, {FrameIndex, Score}) ->
         Pin1 = lists:nth(FrameIndex, Rolls),
         Pin2 = lists:nth(FrameIndex + 1, Rolls),
-        Sum2 = Sum + Pin1 + Pin2,
-        FrameIndex2 = FrameIndex + 2,
-        {FrameIndex2, Sum2}
+        if Pin1 + Pin2 =:= 10 ->
+            {FrameIndex + 2, Score + 10 + lists:nth(FrameIndex + 2, Rolls)}
+        ;  Pin1 + Pin2 =/= 10 ->
+            {FrameIndex + 2, Score + Pin1 + Pin2}
+        end
     end,
     {_, Score} = lists:foldl(F, {1, 0}, lists:seq(1, 10)),
     Score.
