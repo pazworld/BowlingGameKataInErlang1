04c0184e947a0f75cff74a557dbb0f726d5bdb2b
pazworld <paz@moudamepo.net>
2020-10-18 14:53:52 +0900

    Refactor: Use foldl instead of sum

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -5,4 +5,14 @@ new() -> [].
 
 roll(Game, Pins) -> [Pins | Game].
 
-score(Game) -> lists:sum(Game).
+score(Game) ->
+    Rolls = lists:reverse(Game),
+    F = fun(_Frame, {FrameIndex, Sum}) ->
+        Pin1 = lists:nth(FrameIndex, Rolls),
+        Pin2 = lists:nth(FrameIndex + 1, Rolls),
+        Sum2 = Sum + Pin1 + Pin2,
+        FrameIndex2 = FrameIndex + 2,
+        {FrameIndex2, Sum2}
+    end,
+    {_, Score} = lists:foldl(F, {1, 0}, lists:seq(1, 10)),
+    Score.
