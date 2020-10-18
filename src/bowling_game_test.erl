24f77cb305f64a5729dc7cb1f198914a2f63820a
pazworld <paz@moudamepo.net>
2020-10-18 15:02:37 +0900

    Red: Add test for a game with spares

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -11,6 +11,14 @@ all_ones_test() ->
     G2 = roll_many(G, 20, 1),
     ?assertEqual(20, bowling_game:score(G2)).
 
+one_spare_test() ->
+    G = bowling_game:new(),
+    G2 = bowling_game:roll(G, 5),
+    G3 = bowling_game:roll(G2, 5),
+    G4 = bowling_game:roll(G3, 3),
+    G5 = roll_many(G4, 17, 0),
+    ?assertEqual(16, bowling_game:score(G5)).
+
 roll_many(Game, N, Pins) ->
     PinList = lists:duplicate(N, Pins),
     lists:foldl(fun(P, G) -> bowling_game:roll(G, P) end, Game, PinList).
